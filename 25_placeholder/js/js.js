9595e1deac29560c6a62eacc36858f7be33187b8
tsotneforester <tsotne.meladze.usa@gmail.com>
2023-02-09 11:37:56 +0400

    automated

---
+++ 25_placeholder/js/js.js
@@ -0,0 +1,10 @@
+let heading = document.getElementById("heading");
+let avatar = document.getElementById("avatar");
+
+setTimeout(() => {
+  heading.innerHTML = "Improve your front-end skills by building projects";
+  heading.classList.remove("zorg");
+  desc.innerHTML = "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level";
+  desc.classList.remove("zorg");
+  avatar.insertAdjacentHTML("afterbegin", `<img src="images/image-qr-code.png" alt="img">`);
+}, 2600);
