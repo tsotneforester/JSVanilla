689af9359031bca45ff8cfdc2679317ec3e25583
tsotneforester <tsotne.meladze.usa@gmail.com>
2023-04-05 22:20:32 +0400

    automated

---
+++ 35_mytube/README.md
@@ -0,0 +1,9 @@
+# **35 -  Mytube**
+
+![Main](https://gpx.ge/challenge/js/img/35_mytube.gif "image")
+
+---
+```sh
+npm i
+npm start
+```
---
+++ 35_mytube/js/js.js
@@ -0,0 +1,22 @@
+//      ,    ,
+//   ,@▒▒╜  ╣▒║╖     ▄▓█████  ▐██████▄ ▐██▌  ██▓       ▄█████▌  ███████`
+// ╓╣▒▒╜ @╝╣╖`╢▒▒╗   ███"---  ▐██▌-███` █▓█▄███▀      ▐██▌---   ███▌,,
+// ╢▒▒╖  ╢╗@╝  ╢▒▒╝  ███  ███ ▐███████  ▄▓█▓▓█▄       ▐██▌ ▐██▌ ██████
+//  ╙╢▒╢╖ ║╜,╢▒▒╝    ███▄▄███ ▐██▌--`  ▐██▌ ▐███ ▓███ ▐███▄███▌ ███▄▄▄▄
+//    `╢▒╜  ╣▒╜       ▀▀▀▀▀▀▀ '▀▀`     '▀▀`  ▀▀▀ ╙▀▀▀   ▀▀▀▀▀▀  ▀▀▀▀▀▀▀
+"use strict";
+let playPause = document.getElementById("play-pause");
+let progressLine = document.getElementById("progress-line");
+let video = document.getElementById("video");
+playPause.addEventListener("click", function () {
+  if (video.paused) {
+    video.play();
+    playPause.style.backgroundImage = `url("img/pause-regular-36.png")`;
+    video.addEventListener("timeupdate", function () {
+      progressLine.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    });
+  } else {
+    video.pause();
+    playPause.style.backgroundImage = `url("img/play-regular-36.png")`;
+  }
+});
