f6318db95b413ca4878c1a6a1ae8c54e6b4f102c
tsotneforester <tsotne.meladze.usa@gmail.com>
2023-05-15 09:40:48 +0400

    automated

---
+++ 41_tag_maker/js/js.js
@@ -0,0 +1,28 @@
+//      ,    ,
+//   ,@▒▒╜  ╣▒║╖     ▄▓█████  ▐██████▄ ▐██▌  ██▓       ▄█████▌  ███████`
+// ╓╣▒▒╜ @╝╣╖`╢▒▒╗   ███"---  ▐██▌-███` █▓█▄███▀      ▐██▌---   ███▌,,
+// ╢▒▒╖  ╢╗@╝  ╢▒▒╝  ███  ███ ▐███████  ▄▓█▓▓█▄       ▐██▌ ▐██▌ ██████
+//  ╙╢▒╢╖ ║╜,╢▒▒╝    ███▄▄███ ▐██▌--`  ▐██▌ ▐███ ▓███ ▐███▄███▌ ███▄▄▄▄
+//    `╢▒╜  ╣▒╜       ▀▀▀▀▀▀▀ '▀▀`     '▀▀`  ▀▀▀ ╙▀▀▀   ▀▀▀▀▀▀  ▀▀▀▀▀▀▀
+"use strict";
+const textarea = document.getElementById("textarea");
+const tags = document.getElementById("tags");
+//|| Focus cursor |||||
+textarea.focus(); //|||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//||                  Tag Generator                  |||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||
+textarea.addEventListener("input", function (e) {
+  let inputString = e.target.value.trim();
+  //Creates array from text & Filters out empty members
+  let array = inputString.split(",").filter((e) => e);
+  //Generates html node from array and writes it in HTML
+  let html = "";
+  for (let i = 0; i < array.length; i++) {
+    html += `<span class="tag">${array[i]}</span>`;
+  }
+  tags.innerHTML = html;
+  //resets html node
+  html = "";
+});
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||||
