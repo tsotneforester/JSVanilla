b0dea0734fefb596e9d8d61159fdf8b087d45958
tsotneforester <tsotne.meladze.usa@gmail.com>
2023-05-23 19:17:04 +0400

    automated

--- 14_color_gen_2/js/js.js
+++ 14_color_gen_2/js/js.js
@@ -5,36 +5,46 @@
 //  ╙╢▒╢╖ ║╜,╢▒▒╝    ███▄▄███ ▐██▌--`  ▐██▌ ▐███ ▓███ ▐███▄███▌ ███▄▄▄▄
 //    `╢▒╜  ╣▒╜       ▀▀▀▀▀▀▀ '▀▀`     '▀▀`  ▀▀▀ ╙▀▀▀   ▀▀▀▀▀▀  ▀▀▀▀▀▀▀
 "use strict";
-const green = document.getElementById("green");
-const red = document.getElementById("red");
-const blue = document.getElementById("blue");
+const green_in = document.getElementById("green");
+const red_in = document.getElementById("red");
+const blue_in = document.getElementById("blue");
 
 const green_out = document.querySelector(".green-out");
 const red_out = document.querySelector(".red-out");
 const blue_out = document.querySelector(".blue-out");
 const hex_out = document.querySelector(".hex-out");
 
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//             Main function does 2 things            ||
+//1. Generates HEX code                               ||
+//2. sends the code in HTML and sets background color ||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||
+
+function setColor() {
+  let color = `#${toHex(red.value)}${toHex(green.value)}${toHex(blue.value)}`;
+  document.body.style.backgroundColor = color;
+  hex_out.innerText = color;
+}
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+// Triggers main function on every slider input change |||
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 red.addEventListener("input", function () {
-  red_out.innerHTML = red.value;
+  red_out.innerHTML = red_in.value;
   setColor();
 });
 
 green.addEventListener("input", function () {
-  green_out.innerHTML = green.value;
+  green_out.innerHTML = green_in.value;
   setColor();
 });
 
 blue.addEventListener("input", function () {
-  blue_out.innerHTML = blue.value;
+  blue_out.innerHTML = blue_in.value;
   setColor();
 });
-
-function setColor() {
-  document.body.style.backgroundColor = `rgb(${red.value}, ${green.value}, ${blue.value})`;
-  hex_out.innerText = `#${toHex(red.value)}${toHex(green.value)}${toHex(blue.value)}`;
-}
-
+//|||||||||||||||||||||||||||||||||||||||||||||||||||||||
 function toHex(num) {
   let number = parseInt(num).toString(16);
   return number.length == 1 ? "0" + number : number;
 }
+//||||||||||||||||||||||||||||||||||||||||||||||||||||||
