4e503fa168672fcd5e699cd40c07871e3f6676a6
tsotneforester <tsotne.meladze.usa@gmail.com>
2023-08-25 16:44:15 +0400

    automated

--- 30_read_more/src/components/ReadMore.jsx
+++ 30_read_more/src/components/ReadMore.jsx
@@ -1,5 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const animations = {
+  initial: { opacity: 0, scale: 0.2 },
+  animate: { opacity: 1, scale: 1 },
+  transition: {
+    type: "spring",
+    duration: 1.2,
+  },
+};
 
 function ReadMore({ children, ButtonColor, lessWords, startCollapsed, collapseButtonText, expandButtonText }) {
   let [fullText, setFullText] = useState(startCollapsed);
@@ -11,7 +21,7 @@ function ReadMore({ children, ButtonColor, lessWords, startCollapsed, collapseBu
 
   return (
     <>
-      <Container>
+      <Container {...animations}>
         <Span>{fullText ? children : str + "..."}</Span>
         <Button onClick={() => setFullText(!fullText)} color={ButtonColor}>
           &nbsp; {fullText ? collapseButtonText : expandButtonText}
@@ -23,16 +33,18 @@ function ReadMore({ children, ButtonColor, lessWords, startCollapsed, collapseBu
 
 export default ReadMore;
 
-const Container = styled.div`
+const Container = styled(motion.div)`
   width: 400px;
   height: auto;
   border: 2px black solid;
   padding: 16px;
   background-color: #e5dede;
   font-size: 20px;
+  border-radius: 12px;
 `;
 const Span = styled.span``;
 
 const Button = styled.span`
   color: ${(prop) => prop.color};
+  cursor: pointer;
 `;
