92fd4d431c5c88e0923674ef575df3725d0d4447
tsotneforester <tsotne.meladze.usa@gmail.com>
2023-10-19 11:24:45 +0400

    automated

--- 38_movie_db/src/components/Form.jsx
+++ 38_movie_db/src/components/Form.jsx
@@ -21,41 +21,49 @@ export default Form;
 const S = {};
 
 S.form = styled.form`
-  margin: 0 auto;
-  display: inline-block;
+  width: 100%;
+  max-width: 280px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 20px;
+  @media only screen and (min-width: 480px) {
+    flex-direction: row;
+    max-width: 340px;
+  }
 
   & button {
     font-size: 16px;
     color: white;
     font-weight: 600;
     text-align: center;
-    width: 68px;
+    width: 100%;
     height: 42px;
     border-radius: 12px;
-    background-color: blue;
+    background-color: #ffc107;
     padding: 2px 6px;
     transition: all 0.8s;
-    &:hover {
+
+    @media only screen and (min-width: 480px) {
       width: 68px;
-      height: 42px;
-      border-radius: 12px;
-      background-color: rgb(4, 4, 137);
+    }
+
+    &:hover {
+      background-color: #c39406;
     }
   }
 
   & input {
     height: 42px;
     border-radius: 12px;
-    margin-right: 20px;
     text-indent: 20px;
     font-size: 16px;
     color: black;
     font-weight: bold;
     text-align: left;
     border: none;
+    width: 100%;
 
     & ::-moz-placeholder {
       font-style: italic;
